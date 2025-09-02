9fa4f4b75a99feb5f688653d2c387dd8f2d98e44
WeiqiGO0 <hannah.lingzhi@gmail.com>
2025-09-02 14:16:23 +0200

    update to setup
    added my name and a potential start on week 1 assignment

--- experiments/week1.js
+++ experiments/week1.js
@@ -3,4 +3,26 @@ function setup() {
 	background(255, 255, 255);
 }
 
-function draw() {}
+function draw() {
+	const originalX = 300;
+	const originalY = 300;
+	const divider = 20;
+	noiseSeed(20);
+
+	beginShape();
+	// X axis - width
+	for (let x = 0; x < width; x++) {
+		// const y = originalY + Math.random() * 100;
+		const y = originalY + noise(x / divider) * 100;
+		vertex(x, y);
+	}
+
+	// Y axis - height
+	for (let heightY = 0; heightY < height; heightY++) {
+		const x = originalX + noise(heightY / divider) * 100;
+		vertex(x, heightY);
+	}
+	endShape();
+
+	noLoop();
+}
