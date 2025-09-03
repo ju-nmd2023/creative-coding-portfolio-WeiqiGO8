ca027141ab30d4e56b5419b13a616390a7374633
WeiqiGO0 <hannah.lingzhi@gmail.com>
2025-09-03 11:54:31 +0200

    added movement to the line in week1

--- experiments/week1.js
+++ experiments/week1.js
@@ -1,6 +1,7 @@
 function setup() {
 	createCanvas(innerWidth, innerHeight);
 	background(255, 255, 255);
+	frameRate(10);
 }
 
 const originalX = 300;
@@ -8,16 +9,22 @@ const originalY = 300;
 const divider = 20;
 noiseSeed(0);
 
+let counter = 0;
+
 function draw() {
+	clear();
+	background(255, 255, 255);
+
 	beginShape();
 	// X axis line
 	for (let x = 0; x < width; x++) {
 		// const Y = originalY + Math.random() * 100;
-		const y = originalY + noise(x / divider) * 100;
+		const y = originalY + noise(x / divider, counter) * 100;
 		vertex(x, y);
 	}
-
 	endShape();
 
+	counter += 0.1;
+	noFill();
 	noLoop();
 }
