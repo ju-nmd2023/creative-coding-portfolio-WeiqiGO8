82f3b6ba8948bb4a307930efcb33ce79dbff98b0
WeiqiGO8 <hannah.lingzhi@gmail.com>
2025-09-04 08:30:32 +0200

    added two more moving lines
    also added a name and a little more description.

--- experiments/week1.js
+++ experiments/week1.js
@@ -4,25 +4,55 @@ function setup() {
 	frameRate(10);
 }
 
-const originalX = 300;
 const originalY = 300;
 const divider = 20;
 noiseSeed(0);
 
 let counter = 0;
 
-function draw() {
-	clear();
-	background(255, 255, 255);
-
+// TOP X AXIS LINE
+function topLine() {
 	beginShape();
-	// X axis line
 	for (let x = 0; x < width; x++) {
-		// const Y = originalY + Math.random() * 100;
+		// const y = originalY + Math.random() * 100;
+		const y = -100 + originalY + noise(x / divider, counter) * 100;
+		vertex(x, y);
+	}
+	endShape();
+}
+
+// MIDDLE X AXIS LINE
+function middleLine() {
+	beginShape();
+	for (let x = 0; x < width; x++) {
+		// const y = originalY + Math.random() * 100;
 		const y = originalY + noise(x / divider, counter) * 100;
 		vertex(x, y);
 	}
 	endShape();
+}
+
+// BOTTOM X AXIS LINE
+function bottomLine() {
+	beginShape();
+	for (let x = 0; x < width; x++) {
+		// const y = originalY + Math.random() * 100;
+		const y = 100 + originalY + noise(x / divider, counter) * 100;
+		vertex(x, y);
+		endShape();
+	}
+}
+
+function draw() {
+	clear();
+	background(255, 255, 255);
+	stroke(25, random(20, 255), 100);
+
+	topLine();
+	middleLine();
+	bottomLine();
+
+	// Y AXIS LINE
 
 	counter += 0.1;
 	noFill();
