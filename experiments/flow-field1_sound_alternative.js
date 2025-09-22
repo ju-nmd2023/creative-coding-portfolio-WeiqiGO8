823471fe39f0bc94711c55ef12f76285efd311d8
WeiqiGO8 <hannah.lingzhi@gmail.com>
2025-09-22 21:51:50 +0200

    sound works!
    also added more descriptions in the json file

--- experiments/flow-field1_sound_alternative.js
+++ experiments/flow-field1_sound_alternative.js
@@ -1,17 +1,11 @@
 // The following code was inspired by the FLow field example 01 from lecture "CC Complexity", by Bassima and Garritt.
 
+// The following 1 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d1a013-dd90-800d-ad5d-f12c626cecc1
 let angles = [];
 
-function setup() {
-	createCanvas(innerWidth, innerHeight);
-
-	for (let x = 0; x < maxCols; x++) {
-		angles[x] = [];
-		for (let y = 0; y < maxRows; y++) {
-			angles[x][y] = 0;
-		}
-	}
-}
+//The following 2 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d19f73-5590-800d-b3f7-2bdd9ee59d25
+let synth;
+let toneStarted = false;
 
 const fieldSize = 50;
 const fieldSizeHalf = fieldSize / 2;
@@ -21,6 +15,24 @@ const divider = 2;
 
 // The following 1 line of code is taken from ChatGPT 2025-09-15: https://chatgpt.com/share/68c7b01b-d568-800d-a579-17715eb7d691
 const rotationSpeed = 0.1;
+
+function setup() {
+	createCanvas(innerWidth, innerHeight);
+
+	//The following 4 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d19f73-5590-800d-b3f7-2bdd9ee59d25
+	//create synth only when Tone is ready
+	if (typeof Tone !== "undefined") {
+		synth = new Tone.PolySynth(Tone.Synth).toDestination();
+	}
+
+	// The following 6 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d1a013-dd90-800d-ad5d-f12c626cecc1
+	for (let x = 0; x < maxCols; x++) {
+		angles[x] = [];
+		for (let y = 0; y < maxRows; y++) {
+			angles[x][y] = 0;
+		}
+	}
+}
 
 function arrowFlowField() {
 	for (let x = 0; x < maxCols; x++) {
@@ -33,15 +45,19 @@ function arrowFlowField() {
 			rotate(value - frameCount * rotationSpeed);
 
 			if (mouseX < 10) {
+				// The following 1 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d1a013-dd90-800d-ad5d-f12c626cecc1
 				rotate(angles[x][y]);
 			}
 			if (mouseX > 10) {
+				// The following 1 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d1a013-dd90-800d-ad5d-f12c626cecc1
 				rotate(angles[x][y]);
 			}
 			if (mouseY < 10) {
+				// The following 1 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d1a013-dd90-800d-ad5d-f12c626cecc1
 				rotate(angles[x][y]);
 			}
 			if (mouseY > 10) {
+				// The following 1 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d1a013-dd90-800d-ad5d-f12c626cecc1
 				rotate(angles[x][y]);
 			}
 
@@ -62,13 +78,37 @@ function arrowFlowField() {
 	}
 }
 
+//The following 14 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d19f73-5590-800d-b3f7-2bdd9ee59d25
+function mousePressed() {
+	if (!toneStarted && synth) {
+		Tone.start().then(() => {
+			toneStarted = true;
+			console.log("Tone.js started!");
+			synth.triggerAttackRelease("c4", "8n"); //Play a first note
+		});
+	} else if (synth) {
+		//Play random note on click
+		const notes = ["c4", "d#3", "g4", "c#1", "ab4"];
+		const note = random(notes);
+		synth.triggerAttackRelease(note, "8n");
+	}
+}
+
 function mouseMoved() {
+	// The following 7 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d1a013-dd90-800d-ad5d-f12c626cecc1
 	for (let x = 0; x < maxCols; x++) {
 		for (let y = 0; y < maxRows; y++) {
 			const cx = x * fieldSize + fieldSizeHalf;
 			const cy = y * fieldSize + fieldSizeHalf;
 			angles[x][y] = atan2(mouseY - cy, mouseX - cx);
 		}
+	}
+
+	//The following 5 lines of code was taken from ChatGPT 2025-09-22: https://chatgpt.com/share/68d19f73-5590-800d-b3f7-2bdd9ee59d25
+	if (synth && toneStarted) {
+		//map mouse position to pitch
+		const note = map(mouseX, 0, width, 48, 72); //midi note range
+		synth.triggerAttackRelease(Tone.Frequency(note, "midi").toNote(), "16n");
 	}
 }
 
